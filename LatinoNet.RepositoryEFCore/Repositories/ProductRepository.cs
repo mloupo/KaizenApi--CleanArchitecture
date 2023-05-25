b6e13e5d637c54e30bf8239e3b8362d28588bbaf
Martin Sergio Lopez <martin.sergio.lopez@gmail.com>
2023-05-25 01:03:14 -0300

    Actualizacion

---
+++ LatinoNet.RepositoryEFCore/Repositories/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using LatinoNet.Entities.Interfaces;
+using LatinoNet.Entities.POCOs;
+using LatinoNet.RepositoryEFCore.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatinoNet.RepositoryEFCore.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        readonly LatinoNetContext Context;
+        public ProductRepository(LatinoNetContext context) =>
+            Context = context;
+        public void CreateProduct(Product product)
+        {
+            Context.Add(product);
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return Context.Products;
+        }
+    }
+}
