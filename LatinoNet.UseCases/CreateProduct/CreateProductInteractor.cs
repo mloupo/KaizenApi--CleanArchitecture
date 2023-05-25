414ebdbb64eeb6e095eb9c01472967960a1c4446
Martin Sergio Lopez <martin.sergio.lopez@gmail.com>
2023-05-25 01:57:03 -0300

    Puertos de entrada y salida

---
+++ LatinoNet.UseCases/CreateProduct/CreateProductInteractor.cs
@@ -0,0 +1,34 @@
+﻿using LatinoNet.DTOs;
+using LatinoNet.Entities.Interfaces;
+using LatinoNet.Entities.POCOs;
+using LatinoNet.UseCasesPorts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatinoNet.UseCases.CreateProduct
+{
+    public class CreateProductInteractor : ICreateProductInputPort
+    {
+        readonly IProductRepository Repository;
+        readonly IUnitOfWork UnitOfWork;
+        readonly ICreateProductInputPort OutputPort;
+
+        public CreateProductInteractor(IProductRepository repository
+            , IUnitOfWork unitOfWork, ICreateProductInputPort outputPort) =>
+            (Repository, UnitOfWork, OutputPort)=
+            (repository, unitOfWork, outputPort);
+        
+        public async Task Handle(CreateProductDTO product)
+        {
+            Product NewProduct = new Product
+            {
+                Name = product.ProductName
+            };
+            Repository.CreateProduct(NewProduct);
+            await UnitOfWork.SaveChanges();
+        }
+    }
+}
